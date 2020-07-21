0141f15843e90b63b101b60a958e337946106c29
max2556 <46759632+max2556@users.noreply.github.com>
2020-07-21 17:06:23 +0300

    Fix2

--- MyFamily'sCube/functions.js
+++ MyFamily'sCube/functions.js
@@ -15,11 +15,11 @@ function render(renderer, scene, camera) {
 function initThreejs() {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(1, 1, 0); // Set position like this
+    camera.position.set(7, 7, 0); // Set position like this
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1,1,1);
+    var geometry = new THREE.BoxGeometry(5,5,5);
 
     var material = setTextures();
 
